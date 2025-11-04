6ecb0c7991f901b282b8bbf84cf86709d74711bb
mohaproprog <mohaproprog@gmail.com>
2025-11-04 19:15:17 +0000

    added exercise 1

---
+++ react-exercise-one/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import UserCard from './UserCard'
+
+function App() {
+  return (
+    <>
+    <UserCard/>
+    </>
+  )
+}
+
+export default App
---
+++ react-exercise-one/src/UserCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function UserCard() {
+  return (
+    <div>
+        <h1>mohamed elmi</h1>
+        <p>mohaproprog@gmail.com</p>
+    </div>
+  )
+}
+
+export default UserCard
